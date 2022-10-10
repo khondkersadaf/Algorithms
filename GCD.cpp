d92bd0ce28571270e6c1f7e9d351d3692605e375
Sadaf Akhter <110828547+khondkersadaf@users.noreply.github.com>
2022-10-10 10:21:30 +0600

    Update GCD.cpp

--- GCD.cpp
+++ GCD.cpp
@@ -1,21 +1,20 @@
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to return
-// gcd of a and b
-int gcd(int a, int b)
+int gcd(int n, int m)
 {
-	if (a == 0)
-		return b;
-	return gcd(b % a, a);
+	if (n==m){return m;}
+    else if (n>=m) {return gcd(n-m,m);}
+    else {return gcd(n,m-n);}
+
 }
 
-// Driver Code
 int main()
 {
-	int a = 10, b = 15;
+	int n = 9;
+	int m = 6;
+	cout << gcd(n,m);
 
-	cout << "GCD(" << a << ", " << b << ") = " << gcd(a, b)
-		<< endl;
 	return 0;
 }
